47da055c4565204e131951bd832ef8ffbe9bfde8
Thijs <mreeringh@hotmail.com>
2016-03-23 12:14:51 +0100

    Added screens, radar en warnings

---
+++ Weatherwatch/Weatherwatch.Core/Radar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Weatherwatch.Core
+{
+    public class Radar
+    {
+        public string Name { get; private set; }
+        private string _url;
+
+        public Radar(string name, string url)
+        {
+            Name = name;
+            _url = url;
+        }
+
+        public Image getRadarImage()
+        {
+            //TODO load image
+            return null;
+        }
+
+        public bool saveRadar()
+        {
+            //TODO save radar
+            return false;
+        }
+    }
+}
---
+++ Weatherwatch/Weatherwatch.Core/Storage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Weatherwatch.Core
+{
+    public class Storage
+    {
+        private static Storage _instance;
+        private readonly List<Radar> _radarList;
+        private readonly List<Warning> _warningList;
+
+        private Storage()
+        {
+            _warningList = new List<Warning>();
+            _radarList = new List<Radar>();
+        }
+
+        public static Storage GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Storage();
+            }
+
+            return _instance;
+        }
+
+        public void AddRadar(Radar radar)
+        {
+            _radarList.Add(radar);
+        }
+
+        public void AddWarning(Warning warning)
+        {
+            _warningList.Add(warning);
+        }
+
+        public Radar GetRadar(string name)
+        {
+            return _radarList.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Warning GetWarning(string location)
+        {
+            return _warningList.FirstOrDefault(x => x.Location == location);
+        }
+    }
+}
