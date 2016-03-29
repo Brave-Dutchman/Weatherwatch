de3fee88668b0617f85fc7dc65abd7f5e795ffca
Thijs <mreeringh@hotmail.com>
2016-03-29 16:17:58 +0200

    Actuele updated ui

---
+++ Weatherwatch/Weatherwatch.Core/Factory/CommandFactory.cs
@@ -0,0 +1,20 @@
+﻿using Weatherwatch.Core.Commands;
+using Weatherwatch.Core.Controller;
+
+namespace Weatherwatch.Core.Factory
+{
+    public class CommandFactory
+    {
+        public ICommand[] Create()
+        {
+            ReloadRadarsCommand reloadRadarsCommand = new ReloadRadarsCommand(new RadarController());
+            ReloadWarningsCommand reloadWarningsCommand = new ReloadWarningsCommand(new WarningsController());
+            ReloadAllCommand reloadAllCommand = new ReloadAllCommand(new ICommand[] { reloadRadarsCommand, reloadWarningsCommand });
+            SaveRadarsCommand saveRadarsCommand = new SaveRadarsCommand();
+            SaveWarningsCommand saveWarningsCommand = new SaveWarningsCommand();
+            SaveAllCommand saveAllCommand = new SaveAllCommand(new ICommand[] { saveRadarsCommand, saveWarningsCommand });
+
+            return new ICommand[] { reloadRadarsCommand, reloadWarningsCommand, reloadAllCommand, saveRadarsCommand, saveWarningsCommand, saveAllCommand };
+        }
+    }
+}
